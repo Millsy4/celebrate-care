8ccccf4b801a6168bb0b257346aca576f7963436
Christian Mills <millsy4@hotmail.com>
2021-02-16 19:03:50 -0600

    changed model name and edited corresponding APIs

--- server/routes/api/api-routes.js
+++ server/routes/api/api-routes.js
@@ -3,43 +3,43 @@ const util = require("util");
 var db = require("../../models");
 
 // Event List Get Route
-router.route("/api/eventlist").get(function (req, res) {
-  db.eventlist.findAll({}).then(function (dbEventlist) {
-    res.json(dbEventlist);
+router.route("/api/eventtable").get(function (req, res) {
+  db.Eventtable.findAll({}).then(function (dbEventtable) {
+    res.json(dbEventtable);
   });
 });
 
 // Event List Post Route
-router.route("/api/eventlist").post(function (req, res) {
-  db.eventlist.create().then(function (dbEventlist) {
-    res.json(dbEventlist);
+router.route("/api/eventtable").post(function (req, res) {
+  db.Eventlist.create().then(function (dbEventtable) {
+    res.json(dbEventtable);
   });
 });
 
 // Family Code Get Route
 router.route("/api/familycode").get(function (req, res) {
-  db.familycode.findAll({}).then(function (dbFamilycode) {
+  db.Familycode.findAll({}).then(function (dbFamilycode) {
     res.json(dbFamilycode);
   });
 });
 
 // Family Code Post Route
 router.route("/api/familycode").post(function (req, res) {
-  db.familycode.create().then(function (dbFamilycode) {
+  db.Familycode.create().then(function (dbFamilycode) {
     res.json(dbFamilycode);
   });
 });
 
 // User Table Get Route
 router.route("/api/usertable").get(function (req, res) {
-  db.usertable.findAll({}).then(function (dbUsertable) {
+  db.Usertable.findAll({}).then(function (dbUsertable) {
     res.json(dbUsertable);
   });
 });
 
 // User Table Post Route
 router.route("/api/usertable").post(function (req, res) {
-  db.usertable.create().then(function (dbUsertable) {
+  db.Usertable.create().then(function (dbUsertable) {
     res.json(dbUsertable);
   });
 });
