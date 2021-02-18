e97a318038e7ab83868c61a444cc7920e52630f5
Christian Mills <millsy4@hotmail.com>
2021-02-18 10:57:05 -0600

    added gallery component

---
+++ client/src/components/BasicGallery.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import GridListTileBar from '@material-ui/core/GridListTileBar';
+import IconButton from '@material-ui/core/IconButton';
+import StarBorderIcon from '@material-ui/icons/StarBorder';
+import Image1 from "../images/image1.jpg"
+import Image2 from "../images/image2.jpg"
+import Image3 from "../images/image3.jpg"
+import Image4 from "../images/image4.jpg"
+
+const tileData = [
+    {
+        img: Image1,
+        title: 'Image',
+        author: 'author'
+    },
+    {
+        img: Image2,
+        title: 'Image',
+        author: 'author',
+    },
+    {
+        img: Image3,
+        title: 'Image',
+        author: 'author',
+    },
+    {
+        img: Image4,
+        title: 'Image',
+        author: 'author',
+    }
+]
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    overflow: 'hidden',
+    backgroundColor: theme.palette.background.paper,
+  },
+  gridList: {
+    flexWrap: 'nowrap',
+    // Promote the list into his own layer on Chrome. This cost memory but helps keeping high FPS.
+    transform: 'translateZ(0)',
+  },
+  title: {
+    color: theme.palette.primary.light,
+  },
+  titleBar: {
+    background:
+      'linear-gradient(to top, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
+  },
+}));
+
+export default function BasicGallery() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <GridList className={classes.gridList} cols={2.5}>
+        {tileData.map((tile) => (
+          <GridListTile key={tile.img}>
+            <img src={tile.img} alt={tile.title} />
+            <GridListTileBar
+              title={tile.title}
+              classes={{
+                root: classes.titleBar,
+                title: classes.title,
+              }}
+              actionIcon={
+                <IconButton aria-label={`star ${tile.title}`}>
+                  <StarBorderIcon className={classes.title} />
+                </IconButton>
+              }
+            />
+          </GridListTile>
+        ))}
+      </GridList>
+    </div>
+  );
+}
