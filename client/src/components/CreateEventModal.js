572060cb73ac9faa56c2ce1133c96af8dfa12599
Louisa May <mgerwick@gmail.com>
2021-02-19 11:35:43 -0600

    added create event modal

---
+++ client/src/components/CreateEventModal.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade';
+import Button from '@material-ui/core/Button';
+import Icon from "@material-ui/core/Icon";
+
+const useStyles = makeStyles((theme) => ({
+    modal: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    paper: {
+        backgroundColor: theme.palette.background.paper,
+        border: '2px solid #000',
+        boxShadow: theme.shadows[5],
+        padding: theme.spacing(2, 4, 3),
+    },
+}));
+
+export default function CreateEventModal() {
+    const classes = useStyles();
+    const [open, setOpen] = React.useState(false);
+
+    const handleOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    return (
+        <div>
+            <Button size="small" color="primary" onClick={handleOpen}>
+                <Icon>add_circle</Icon>
+            </Button>
+
+            <Modal
+                aria-labelledby="transition-modal-title"
+                aria-describedby="transition-modal-description"
+                className={classes.modal}
+                open={open}
+                onClose={handleClose}
+                closeAfterTransition
+                BackdropComponent={Backdrop}
+                BackdropProps={{
+                    timeout: 500,
+                }}
+            >
+                <Fade in={open}>
+                    <div className={classes.paper}>
+                        <h2 id="transition-modal-title">Family Code</h2>
+                        <p id="transition-modal-description">Copy this code and share it with your family members</p>
+                        <h2>Family Code here</h2>
+                        <Button size='small' variant='contained' color='primary' type="button" onClick={handleClose}>
+                            Copy your code
+      </Button>
+                    </div>
+                </Fade>
+            </Modal>
+        </div>
+    );
+}
