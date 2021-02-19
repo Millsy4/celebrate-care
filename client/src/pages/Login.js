5630c4645b19d3def6158c747152e2cf98872bde
Louisa May <mgerwick@gmail.com>
2021-02-18 23:02:49 -0600

    look it works

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,37 +1,20 @@
 import React from 'react';
-// import { useUserContext } from '../services/userContext';
+import { useUserContext } from '../services/userContext';
 import Box from '@material-ui/core/Box';
 import BasicTextFields from '../components/BasicTextFields';
 import Button from '@material-ui/core/Button';
 
 
-function LoginInPage() {
-    // const { user, setUser } = useUserContext()
-    // console.log(user);
-    // const { email, password } = user;
+export default function LoginInPage() {
+    const { user, setUser } = useUserContext()
 
-    // function handleChange(e) {
-    //     const { name, value } = e.target;
-    //     setInputs(inputs => ({ ...inputs, [name]: value }));
-    // }
-
-    // function handleSubmit(e) {
-    //     e.preventDefault();
-
-    //     setSubmitted(true);
-    //     if (email && password) {
-    //         //get return url from location state or default to home page
-    //         const { from } = location.state || { from: { pathname: '/' } };
-    //         dispatch(userActions.login(email, password, from));
-    //     }
-    // }
     return (
         <div className='login-wrapper'>
             <Box>
                 <h1>Sign In</h1>
                 <form id='login' >
 
-                    <BasicTextFields label="Email Address" id='email' type='email' ></BasicTextFields>
+                    <BasicTextFields label="Email Address" id='email' type='email'  ></BasicTextFields>
                     <BasicTextFields label="Password" id='password' type='password' ></BasicTextFields>
                     <Button size='large' variant='contained' color='primary' type="button">
                         Sign In
@@ -42,6 +25,6 @@ function LoginInPage() {
     )
 };
 
-export default LoginInPage();
+
 
 
--- client/src/services/userContext.js
+++ client/src/services/userContext.js
@@ -3,7 +3,11 @@ import React, { createContext, useState, useContext } from 'react';
 const UserContext = createContext();
 
 function UserProvider({ ...props }) {
-    const [user, setUser] = useState(
+    const [user, setUser] = useState({
+        email: '',
+        password: '',
+        familycode: ''
+    }
     );
     return (
         <UserContext.Provider value={{ user, setUser }} {...props} />
