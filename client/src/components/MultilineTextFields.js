e1be811b09d824ac3ec680079a92e447e2530f8e
Louisa May <mgerwick@gmail.com>
2021-02-19 19:49:22 -0600

    added MultilineTextFields.js

---
+++ client/src/components/MultilineTextFields.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        '& .MuiTextField-root': {
+            margin: theme.spacing(1),
+            width: '25ch',
+        },
+    },
+}));
+
+export default function MultilineTextFields() {
+    const classes = useStyles();
+
+    return (
+        <TextField
+            id="filled-multiline-static"
+            label="Enter your event details here"
+            multiline
+            rows={8}
+            variant="filled"
+        />
+    );
+}
