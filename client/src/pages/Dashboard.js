4c51310d84bb5bb93236a589d4cec170a6c9e0ae
Christian Mills <millsy4@hotmail.com>
2021-02-28 13:48:30 -0600

    changes

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -154,7 +154,6 @@ export default function Dashboard() {
             <font color="#EA7A57">Event Ideas</font>
           </h1>
         </Grid>
-        <BasicGallery />
         {/* <Footer /> */}
       </Grid>
     </div>
